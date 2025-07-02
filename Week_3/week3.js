be68b82305920a3335274392919b850b7f0a4b37
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-07-02 09:25:10 +0530

    Added New Features

--- Week_3/week3.js
+++ Week_3/week3.js
@@ -1,10 +1,17 @@
 const buttons = document.querySelectorAll('.Done');
+const list = document.querySelectorAll('.list')
+
 buttons.forEach(button=>
 
     button.addEventListener('click', event=>{
+        const task = event.target.parentElement;
         if(event.target.textContent==='Mark as Done'){
-        event.target.textContent = 'Done';
+        event.target.textContent = 'Done'; 
+      
+
+
         }
+ 
         else{
             event.target.textContent = "Mark as Done";
         }
@@ -16,15 +23,28 @@ function addTask(){
     newButton.textContent="Mark as Done";
     newTask.classList='button';
     const tasklist = document.getElementById("tasklist");
-    newTask.classList.add('add')
+    newTask.classList.add('list')
     tasklist.appendChild(newTask);
     newButton.classList.add('button');
-    newButton.classList='button';
+    newButton.classList='Done';
     tasklist.appendChild(newButton);
+    newButton.addEventListener('click', event=>{
+        if(event.target.textContent==='Mark as Done'){
+            event.target.textContent = 'Done';
+            newTask.append('✔️');
+           
+        }
+        else{
+            event.target.textContent = "Mark as Done";
+            newTask.removeChild(newTask.lastChild);
+          
+        }
+    })
 
 
 }
 function removeTask(){
+
 
 }
 
