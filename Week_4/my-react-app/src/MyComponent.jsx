212f129c3f2f597f48163f1e0155cedf4dcc7fd4
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-07-15 13:15:00 +0530

    React App

--- Week_4/my-react-app/src/MyComponent.jsx
+++ Week_4/my-react-app/src/MyComponent.jsx
@@ -28,12 +28,12 @@ function MyComponent(){
         </div>
         <div className="item"> 
             <img id="football" src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSSTIKGKXU1h_slm7K8-FhQyF6nZbu8TsbfDYKXIWgjFfOrxGflfuIKpXDEq0rsVheOJoZc-f_MyZMwbbY9eiNt9qAVzG3mGX_aMgVtOYw8CNc2ydELmfOsVw" alt="" />
-            <p>Football Studs </p>
+            <p >Football Studs </p>
             <button className="add" onClick={()=>addItem("Football Studs")}>Add to Cart</button>
         </div>
         <div className="item"> 
             <img id="football" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRphOCTLXwH8bYtjEWffW6jK43lf8ZttaJPnq6w_fWif2aXCVSwEBqbsIVW8L5DbhISAHT7GcCdewjEb-bYVWWzGhiOIYx8iwBbyKu3mLfnD3ykKAMzL19R-Q" alt="" />
-            <p>Goalkeeper Gloves</p>
+            <p >Goalkeeper Gloves</p>
             <button className="add"onClick={()=>addItem("Goalkeeper Gloves")}>Add to Cart</button>
         </div>
     </div>
@@ -42,11 +42,8 @@ function MyComponent(){
         <ol>
         {item.map((itemName, index)=>(
             <li key={index}>
-                <span>{itemName}</span>
-                <button className="b" onClick={()=>deletebutton(index)}>Delete</button>
-               
-     
-              
+                <span className="i">{itemName}</span>
+                <button className="b" onClick={()=>deletebutton(index)}>Delete</button>             
                 
             </li>
             
