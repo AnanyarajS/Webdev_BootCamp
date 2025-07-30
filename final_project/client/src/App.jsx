113b4c85974b6bf24f81243605be061c14094002
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-07-31 02:09:45 +0530

    Final Project

--- final_project/client/src/App.jsx
+++ final_project/client/src/App.jsx
@@ -3,15 +3,21 @@ import './App.css'
 import Nav from './Nav.jsx'
 import Body from './Body.jsx';
 import Footer from './Footer.jsx';
+import AboutUs from './pages/AboutUs.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <>
+    <><Router>
      <Nav/>
+     <Routes>
+      <Route path="/aboutus" element = {<AboutUs/>}/>
+     </Routes>
      <Body/>
      <Footer/>
+     </Router>
     </>
   )
 }
--- final_project/server/index.js
+++ final_project/server/index.js
@@ -16,9 +16,10 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.get('/{*splat}', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
+
 
 app.listen(PORT, ()=>{
     console.log("Server is running!");
